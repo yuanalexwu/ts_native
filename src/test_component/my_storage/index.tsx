a1f997631dead452493cf4c72ac830492f209eb6
yuanalexwu <yuanalexwu@gmail.com>
2018-06-21 14:56:30 +0800

    🚧 WIP need to add types for the node_module which does not have a typescript definition file

---
+++ src/test_component/my_storage/index.tsx
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018. Suzhou DHMS Information Technology Co.,Ltd.
+ * Author: Wuyuan Created: 2018-6-21
+ */
+import * as React from 'react'
+import {
+    View,
+    Text,
+} from 'react-native'
+import store from 'react-native-simple-store'
+
+class MyStorage extends React.Component {
+    componentDidMount () {
+    }
+
+    render () {
+        return (
+            <View>
+                <Text>Test Storage</Text>
+            </View>
+        )
+    }
+}
---
+++ types/react-native-simple-store/index.d.ts
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2018. Suzhou DHMS Information Technology Co.,Ltd.
+ * Author: Wuyuan Created: 2018-6-21
+ */
+
